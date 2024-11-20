c07ada3843ac4c19496beca0bea235e2cd4fdda5
mkranz <mkranz@gmail.com>
2024-11-20 09:23:03 +0800

    cleanup vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,36 +3,9 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import { VitePWA } from 'vite-plugin-pwa'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import fs from 'fs';
-import path from 'path';
-import child_process from 'child_process';
-import { env } from 'process';
-
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
-
-const certificateName = "cpe.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
-
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7150';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -51,7 +24,13 @@ export default defineConfig(({ mode }) => {
             }),
             quasar({
                 sassVariables: '/src/quasar-variables.sass'
-            })
+            }),
+            VitePWA({
+                registerType: 'autoUpdate',
+                /* Uncomment to enable PWA in development */
+                /*devOptions: { enabled: true }*/
+            }),
+            basicSsl()
         ],
         resolve: {
             alias: {
@@ -59,21 +38,9 @@ export default defineConfig(({ mode }) => {
             }
         },
         server: {
-            proxy: {
-                '^/api/': {
-                    target,
-                    secure: false,
-                    configure: (proxy) => {
-                        proxy.on('proxyReq', (_proxyReq, req, res) => {
-                            console.log('Sending Request to the Target:', req.method, req.url, res.statusCode);
-                        });
-                    }
-                }
-            },
             port: 5173,
             https: {
-                key: fs.readFileSync(keyFilePath),
-                cert: fs.readFileSync(certFilePath),
+                
             },
             fs: {
                 allow: ['..']
