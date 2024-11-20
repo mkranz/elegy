1ae170f00206d093fffd788037d3ecbf18585200
mkranz <mkranz@gmail.com>
2024-11-20 15:33:33 +0800

    remove pregenerated PWA manifest

--- vite.config.ts
+++ vite.config.ts
@@ -34,13 +34,13 @@ export default defineConfig(({ mode }) => {
                     background_color: '#8b0000',
                     icons: [
                         {
-                            src: "/web-app-manifest-192x192.png",
+                            src: "/elegy/web-app-manifest-192x192.png",
                             sizes: "192x192",
                             type: "image/png",
                             purpose: "any maskable"
                         },
                         {
-                            src: "/web-app-manifest-512x512.png",
+                            src: "/elegy/web-app-manifest-512x512.png",
                             sizes: "512x512",
                             type: "image/png",
                             purpose: "any maskable"
